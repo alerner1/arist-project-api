70f78b398e34f4356f5263ed58610bb635c7a9ae
alerner1 <alyssaplerner@gmail.com>
2021-02-17 14:01:56 -0500

    added all other routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this ile, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     post '/register', to: 'devices#create'
+    post '/alive' to: 'devices#alive'
+    post '/report' to: 'devices#report'
+    patch '/terminate' to: 'devices#update'
   end
 end
