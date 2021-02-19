45139514ebaaa41d8876fcc958d19db5fadf26e7
alerner1 <alyssaplerner@gmail.com>
2021-02-18 20:24:38 -0500

    added tests for post /api/alive

--- app/controllers/api/devices_controller.rb
+++ app/controllers/api/devices_controller.rb
@@ -2,24 +2,34 @@
 # adjust alive and report to use nested attributes instead?
 # figure out private methods for nested attributes
 # private method for find
-# maybe rename methods to match routes rather than rest
 
 module Api
   class DevicesController < ApplicationController
-
     def register
       @device = Device.create(devices_params)
       if @device.valid?
         render json: @device, status: :created
       else
-        render json: { error: 'failed to register device' }, status: :not_acceptable
+        render json: { error: 'failed to register device' }, status: :internal_server_error
       end
     end
 
     def alive
-      device = Device.find(params[:device_id])
-      @heartbeat = Heartbeat.create(device: device)
-      render json: @heartbeat
+      if Device.exists?(id: params[:device_id])
+        if check_authorization(params[:device_id])
+          device = find_device(params[:device_id])
+          @heartbeat = Heartbeat.create(device: device)
+          if @heartbeat.valid?
+            render json: {}, status: :created
+          else
+            render json: { error: 'failed to create heartbeat' }, status: :internal_server_error
+          end
+        else
+          render json: { error: 'device has been terminated' }, status: :internal_server_error
+        end
+      else
+        render json: { error: 'device not found' }, status: :internal_server_error
+      end
     end
 
     def report
@@ -44,13 +54,13 @@ module Api
       params.require(:device).permit(:phone_number, :carrier)
     end
 
-    def find_device
+    def find_device(device_id)
       device = Device.find(device_id)
     end
 
     def check_authorization(device_id)
-      find_device
-      render json: { error: 'device has been terminated' }, status: :internal_server_error unless device.disabled_at == nil
+      device = find_device(device_id)
+      device.disabled_at == nil ? true : false
     end
   end
 end
--- spec/controllers/devices_controller_spec.rb
+++ spec/controllers/devices_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::DevicesController, type: :request do
-  let(:device) do
+  let(:valid_device) do
     {
       device: {
         carrier: "Verizon",
@@ -10,31 +10,69 @@ RSpec.describe Api::DevicesController, type: :request do
     }
   end
 
+  let(:invalid_device) do
+    {
+      device: {
+        carrier: "Verizon",
+        phone_number: "asodfijawon"
+      }
+    }
+  end
+
   describe "post /api/register" do 
     it "creates a new device" do
-      post '/api/register', params: device
+      post '/api/register', params: valid_device
       expect(response).to have_http_status(201)
     end
 
     it "responds with the device id" do
-      post '/api/register', params: device
+      post '/api/register', params: valid_device
       expect(JSON.parse(response.body)).to have_key("id")
       expect(JSON.parse(response.body)["id"]).to_not be_empty
     end
 
-    # it "doesn't work if disabled_at is not nil" do
-    #   disabled_device = {
-    #     device: {
-    #       carrier: "Verizon",
-    #       phone_number: "+12345556789",
-    #       disabled_at: DateTime.now
-    #     }
-    #   }
-      
-    #   post '/api/register', params: disabled_device
-    #   expect(response).to have_http_status(500)
-    # end
-  end
+    it "requires a phone number in a format compatible with phonelib" do
+      post '/api/register', params: invalid_device
+      expect(response).to have_http_status(500)
+    end
 
+    it "responds with an error message when invalid" do
+      post '/api/register', params: invalid_device
+      expect(JSON.parse(response.body)).to have_key("error")
+      expect(JSON.parse(response.body)["error"]).to eq("failed to register device")
+    end
+
+  end
   
+  describe "post /api/alive" do
+    let(:valid_device) do 
+      Device.create(carrier: "Verizon", phone_number: "+12345556789")
+    end
+
+    let(:disabled_device) do
+      Device.create(carrier: "Verizon", phone_number: "+12345556789", disabled_at: DateTime.now)
+    end
+
+    it "creates a new heartbeat" do
+      post '/api/alive', params: { device_id: valid_device.id }
+      expect(response).to have_http_status(201)
+    end
+
+    it "responds with an empty object" do
+      post '/api/alive', params: { device_id: valid_device.id }
+      expect(JSON.parse(response.body)).to be_empty
+    end
+
+    
+    it "doesn't work if disabled_at is not nil" do
+      post '/api/alive', params: { device_id: disabled_device.id }
+      expect(response).to have_http_status(500)
+    end
+
+    it "responds with an error message if the device id is invalid" do
+      post '/api/alive', params: { device_id: 'alsdkfjaowin' }
+      expect(JSON.parse(response.body)).to have_key("error")
+      expect(JSON.parse(response.body)["error"]).to eq("device not found")
+    end
+  end  
 end
